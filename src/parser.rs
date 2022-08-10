82d074549e53bf60873adcd43cacc08728f2b15e
Joachim Sand <joachimliptrotsand@gmail.com>
2022-08-10 23:27:25 +0200

    Progress on getting prefix/postfix expressions working.

--- src/parser.rs
+++ src/parser.rs
@@ -4,6 +4,7 @@ use crate::lexer::*;
 #[derive(Debug)]
 pub enum ParserError {
     NotImplemented,
+    UnexpectedToken(Token),
     EarlyLexerTermination,
     UnknownPrecedence,
     UnexpectedOperator,
@@ -16,13 +17,6 @@ impl fmt::Display for ParserError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
         write!(f, "Parser Error: {}", self.to_string())
     }
-}
-
-#[derive(Clone, Debug)]
-pub enum TerminalType {
-    Identifier(String),
-    IntegerLiteral(i64),
-    DoubleLiteral(f64),
 }
 
 type NodePtr = Box<Node>;
@@ -40,12 +34,20 @@ pub struct UnaryNode {
     operand : NodePtr,
 }
 
+#[derive(Clone, Debug)]
+pub struct ArrayIndexNode {
+    lvalue : NodePtr,
+    index : NodePtr,
+}
 
 #[derive(Clone, Debug)]
 pub enum Node { 
     Binary(BinaryNode),
-    Unary(UnaryNode),
-    Terminal(TerminalType),
+    Prefix(UnaryNode),
+    Postfix(UnaryNode),
+
+    ArrayIndex(ArrayIndexNode),
+    Terminal(Token),
     Empty,
 }
 
@@ -56,14 +58,17 @@ struct OperatorInfo {
 }
 
 pub fn print_ast(start : NodePtr) {
-    print!("Node: {:?}", start.deref());
+    println!("Node: {:?}", start.deref());
 
     match *start {
         Node::Binary(node) => {
             print_ast(node.left);
             print_ast(node.right);
         }
-        Node::Unary(node) => {
+        Node::Prefix(node) => {
+            print_ast(node.operand);
+        }
+        Node::Postfix(node) => {
             print_ast(node.operand);
         }
         _ => return,
@@ -79,18 +84,83 @@ fn operator_lookup(token : &Token) -> Result<OperatorInfo, ParserError> {
         Token::Plus | Token::Minus => {
             return Result::Ok(OperatorInfo{precedence : 80, left_associative : true});
         }
-        
         _ => return Result::Err(ParserError::UnknownPrecedence),
     }
 }
 
 
+fn expect_token(lexer : &mut Peekable<Lexer>, expect : Token) -> Result<(), ParserError> {
+    let cur_token = lexer.next().ok_or(ParserError::EarlyLexerTermination)?;
 
-fn parse_postfix(lexer : &mut Peekable<Lexer>) -> Result<NodePtr, ParserError> {
-    
+    if cur_token != expect {
+        return Ok(());
+    } else {
+        return Err(ParserError::UnexpectedToken(cur_token));
+    }
 }
 
+fn parse_type(lexer : &mut Peekable<Lexer>) -> Result<Token, ParserError> {
+    let cur_token = lexer.next().ok_or(ParserError::EarlyLexerTermination)?;
+    match cur_token {
+        Token::Float | Token::Int | Token::Double => {
+            return Ok(cur_token);
+        }
+        _ => {
+            // Handle user defined types
+            return Err(ParserError::NotImplemented);
+        }
+    }
+}
 
+// primary_expression
+// 	: IDENTIFIER
+// 	| CONSTANT
+// 	| STRING_LITERAL
+// 	| '(' expression ')'
+// 	;
+fn parse_primary(lexer : &mut Peekable<Lexer>) -> Result<NodePtr, ParserError>{
+    let cur_token = lexer.next().ok_or(ParserError::EarlyLexerTermination)?;
+    match cur_token {
+        Token::Identifier(_) | Token::IntegerLiteral(_) => {
+            return Ok(Box::new(Node::Terminal(cur_token)));
+        }
+
+        Token::LParen => {
+            let expr = parse_expression(lexer, 0)?;
+            expect_token(lexer, Token::RParen)?; 
+            return Ok(expr);
+        }
+
+        //TODO: String literal
+        _ => return Err(ParserError::UnexpectedToken(cur_token)),
+    }  
+}
+
+// postfix_expression
+// 	: primary_expression
+// 	| postfix_expression '[' expression ']'
+// 	| postfix_expression '(' ')'
+// 	| postfix_expression '(' argument_expression_list ')'
+// 	| postfix_expression '.' IDENTIFIER
+// 	| postfix_expression PTR_OP IDENTIFIER
+// 	| postfix_expression INC_OP
+// 	| postfix_expression DEC_OP
+fn parse_postfix(lexer : &mut Peekable<Lexer>) -> Result<NodePtr, ParserError> {
+    // The common left-recursion issue can be circumvented by realizing that
+    // a postfix expression must begin with a primary expression
+    let primary = parse_primary(lexer)?;
+    let cur_token = lexer.next().ok_or(ParserError::EarlyLexerTermination)?;
+    match cur_token {
+        Token::LBracket => {
+            let index = parse_expression(lexer, 0)?;
+            expect_token(lexer, Token::RBracket)?;
+            let node = Node::ArrayIndex(ArrayIndexNode { lvalue : primary, index });
+            return Ok(NodePtr::new(node));
+        }
+        
+        _ => return Err(ParserError::UnexpectedToken(cur_token)),
+    }
+}
 
 // unary_expression
 // 	: postfix_expression
@@ -101,21 +171,24 @@ fn parse_postfix(lexer : &mut Peekable<Lexer>) -> Result<NodePtr, ParserError> {
 // 	| SIZEOF '(' type_name ')'
 fn parse_unary(lexer : &mut Peekable<Lexer>) -> Result<NodePtr, ParserError>{
    
-    let token = lexer.next().ok_or(ParserError::EarlyLexerTermination)?;
+    //TODO: This needs to be a peek.
+    let token = lexer.peek().ok_or(ParserError::EarlyLexerTermination)?;
     match token {
         Token::IntegerLiteral(n) => {
-            let node = Node::Terminal(TerminalType::IntegerLiteral(n));
+            lexer.next();
+            let node = Node::Terminal(Token::IntegerLiteral(*n));
             return Ok(Box::new(node));
         }
 
         Token::Increment | Token::Decrement | Token::Ampersand | Token::Asterisk | 
         Token::Plus | Token::Minus | Token::Negation => {
-            //TODO: casting
+            //TODO: casting. Currently this calls a unary expression
+            // but a unary expression does not allow for casting.
             let operand = parse_unary(lexer)?;
 
-            let node = Node::Unary(
+            let node = Node::Prefix(
                 UnaryNode { 
-                    operator : token,
+                    operator : *token,
                     operand,
                 });
             
@@ -123,25 +196,42 @@ fn parse_unary(lexer : &mut Peekable<Lexer>) -> Result<NodePtr, ParserError>{
         }
 
         Token::Sizeof => {
-            let peek = lexer.next().ok_or(ParserError::EarlyLexerTermination)?;
-            if peek == Token::LParen {
+            // TODO: Evaluate sizeof as a constant immediately!
+            let cur_token = lexer.next().ok_or(ParserError::EarlyLexerTermination)?;
+            if cur_token == Token::LParen {
+                // Sizeof on type
+                return Err(ParserError::NotImplemented);
+                /*
+                let type_token = parse_type(lexer)?;
+                expect_token(lexer, Token::RParen);
 
-                //TODO: Parse type name
-                return Err(ParserError::NotImplemented); 
+                let prefix_node = Node::Prefix(
+                        UnaryNode {
+                            operator : Token::Sizeof,
+                            operand : type_token
+                        }
+                    )
+
+                return Ok( NodePtr::new( Node::Prefix(Node::Terminal(type_token))));
+                */
+
             } else {
-                
-                let node = Node::Unary(
+                // Sizeof also works on expressions! e.g. sizeof 123
+                let operand = parse_unary(lexer)?;
+
+                let node = Node::Prefix(
                     UnaryNode { 
-                        operator : token,
+                        operator : *token,
                         operand,
                     });
                 
                 return Ok(Box::new(node));
             }
-
         }
 
-        _ => Err(ParserError::UnexpectedOperand),
+        _ => {
+            return parse_postfix(lexer);
+        }
     }
 }
 
